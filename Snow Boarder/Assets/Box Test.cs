0d0dfd9a708bf1352475f17f510eb417d4adbb1f
Riverman459 <riverjthomas@gmail.com>
2024-01-06 21:52:35 -0500

    Box test 2

--- Snow Boarder/Assets/Box Test.cs
+++ Snow Boarder/Assets/Box Test.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class BoxTest : MonoBehaviour
 {
-    [SerializeField] float changeSize = (29.9f, 39.9f, 15.5f);
     // Start is called before the first frame update
     void Start()
     {
